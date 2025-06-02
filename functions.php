c631aeea713a14e401b197266c6e0766dbf9ba54
Arewee <90697787+Arewee@users.noreply.github.com>
2025-06-02 11:41:09 +0200

    add css variables and dark mode support

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+function easybiz_enqueue_styles() {
+    wp_enqueue_style('easybiz-style', get_stylesheet_uri(), array(), '1.0.1');
+}
+add_action('wp_enqueue_scripts', 'easybiz_enqueue_styles');
+
 function easybiz_setup() {
     add_theme_support('title-tag');
 }
